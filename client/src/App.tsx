5e5e234e69b0860a5a13fcec4d77d06a916cef63
Helena Adamkova <adamkova.helena.ghb@gmail.com>
2022-03-25 10:07:48 +0000

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import './App.css';
+import { Login } from './Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+
+    </div>
+  );
+}
+
+export default App;
